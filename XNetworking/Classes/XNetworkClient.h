4ffa8c1053644f599b9facc021eecfd007bcec9d
陈永叙 <chenyx@aicaigroup.com>
2020-06-10 10:32:02 +0800

    初始化组件

---
+++ XNetworking/Classes/XNetworkClient.h
@@ -0,0 +1,16 @@
+//
+//  XNetworkClient.h
+//  Pods-XNetworking_Example
+//
+//  Created by 陈永叙 on 2020/6/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XNetworkClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XNetworking/Classes/XNetworkClient.m
@@ -0,0 +1,12 @@
+//
+//  XNetworkClient.m
+//  Pods-XNetworking_Example
+//
+//  Created by 陈永叙 on 2020/6/10.
+//
+
+#import "XNetworkClient.h"
+
+@implementation XNetworkClient
+
+@end
