069f61fb784dee78e11fa001ea2e36198236729a
陈永叙 <chenyx@aicaigroup.com>
2020-06-10 10:51:29 +0800

    增加XNetwork.h

---
+++ XNetwork.h
@@ -0,0 +1,16 @@
+//
+//  XNetwork.h
+//  Pods-XNetworking_Example
+//
+//  Created by 陈永叙 on 2020/6/10.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XNetwork : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XNetwork.m
@@ -0,0 +1,12 @@
+//
+//  XNetwork.m
+//  Pods-XNetworking_Example
+//
+//  Created by 陈永叙 on 2020/6/10.
+//
+
+#import "XNetwork.h"
+
+@implementation XNetwork
+
+@end
